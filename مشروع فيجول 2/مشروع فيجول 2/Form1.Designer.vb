c80212366917c9453c30dd2d6592f4c2322da5d0
Alwfy-Sadq <kkvklvovpvk@gmail.com>
2025-09-28 01:25:22 +0300

    Add files via upload

---
+++ مشروع فيجول 2/مشروع فيجول 2/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar
+        Me.Label1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.Label1.Location = New System.Drawing.Point(111, 263)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 30)
+        Me.Label1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(111, 236)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(180, 24)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGray
+        Me.Button1.Location = New System.Drawing.Point(111, 56)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(180, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "حول المشروع"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightGray
+        Me.Button2.Location = New System.Drawing.Point(111, 92)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(180, 30)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "خروج"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.LightGray
+        Me.Button3.Location = New System.Drawing.Point(111, 128)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(180, 30)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "صندوق إدخال الألوان"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.LightGray
+        Me.Button4.Location = New System.Drawing.Point(111, 164)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(180, 30)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "الألوان العشوائية"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.LightGray
+        Me.Button5.Location = New System.Drawing.Point(111, 200)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(180, 30)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "الكتابة على عدة اسطر"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(403, 321)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ مشروع فيجول 2/مشروع فيجول 2/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Dim x, y, w, k As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'حول المشروع
+        w = Chr(13) + Chr(10)
+        x = " جامعة شبوة " + w
+        x = x + " مركز التدريب وخدمة المجتمع " + w
+        x = x + " بكالوريوس الحاسب الآلي " + w
+        x = x + " إعداد الأستاذ حذيفة عبدالرحمن " + w
+        x = x + " العام الدراسي 1447 "
+        y = " حول المشروع "
+        MsgBox(x, MsgBoxStyle.Information + MsgBoxStyle.MsgBoxRight, y)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' خروج
+        x = " هل تريد الخروج من المشروع؟ "
+        y = " تحذير "
+        k = MsgBox(x, MsgBoxStyle.YesNo + MsgBoxStyle.Critical +
+        MsgBoxStyle.MsgBoxRight, y)
+        If k = 6 Then End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' الألوان إدخال صندوق
+        i = InputBox(("15 الرجا إدخال عدد بين الصفر و صندوق الإدخال "))
+        'الإجراء من الخروج
+        If i >= 0 And i <= 15 Then
+            Me.BackColor = System.Drawing.ColorTranslator.FromOle(QBColor(Val(i)))
+        Else
+            MsgBox(" الادخال خاطئ ", , "تنبيه ")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'الألوان العشوائية
+        Randomize()
+        Me.BackColor = System.Drawing.ColorTranslator.FromOle(RGB(Rnd() *
+        255, Rnd() * 255, Rnd() * 255))
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' الكتابة على عدة اسطر
+        w = Chr(13) + Chr(10)
+        TextBox1.Text = " الرحمن الرحيم " + w + " بسم الله "
+        Label1.Text = " بسم الله  " + w + " الرحمن الرحيم "
+    End Sub
+
+    Dim i As Integer
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
